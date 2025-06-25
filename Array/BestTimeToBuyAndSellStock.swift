e026ef026b751af50e4954e6473bb2de44f4ff92
Yul Kang <yulkang2019@gmail.com>
2025-06-25 23:27:34 +0900

    Add: 121. Best Time to Buy and Sell Stock

---
+++ Array/BestTimeToBuyAndSellStock.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func maxProfit(_ prices: [Int]) -> Int {
+        var maxProfit = 0
+        var l = 0
+        var r = 1
+
+        while r < prices.count {
+            if prices[l] <= prices[r] {
+                maxProfit = max(maxProfit, prices[r] - prices[l])
+            } else {
+                l = r
+            }
+
+            r += 1
+        }
+        
+        return maxProfit
+    }
+}
