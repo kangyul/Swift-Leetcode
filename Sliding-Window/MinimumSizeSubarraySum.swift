ac15e29814859be5d6a236fb157d2445eb6ae557
Yul Kang <yulkang2019@gmail.com>
2025-09-25 17:50:08 +0900

    Refactor: 209. Minimum Size Subarray Sum

--- Sliding-Window/MinimumSizeSubarraySum.swift
+++ Sliding-Window/MinimumSizeSubarraySum.swift
@@ -1,22 +1,17 @@
 class Solution {
     func minSubArrayLen(_ target: Int, _ nums: [Int]) -> Int {
-        var l = 0
-        var total = 0
-        var res = Int.max
+        var l = 0, sum = 0
+        var result = Int.max
 
         for r in 0..<nums.count {
-            total += nums[r]
-            while total >= target {
-                res = min(res, (r - l + 1))
-                total -= nums[l]
+            sum += nums[r]
+            while sum >= target {
+                result = min(result, r - l + 1)
+                sum -= nums[l]
                 l += 1
             }
         }
 
-        if res == Int.max {
-            return 0
-        } else {
-            return Int(res)
-        }
+        return result == Int.max ? 0 : result
     }
 }
